a63e7128dfd58ae7bd7919b602ef4d54072309c6
theo <theo.arnal@ynov.com>
2024-06-25 15:54:31 +0200

    Kata 2 sixteenth commit

--- kata2/main.ts
+++ kata2/main.ts
@@ -1,6 +1,10 @@
 export function stringCalculator(string: string) {
     if (!string) return 0;
 
+    if (string.endsWith(',') || string.endsWith('\n')) {
+        throw new Error('error');
+    }
+
     return string
         .replace(/\n/g, ',')
         .split(',')
