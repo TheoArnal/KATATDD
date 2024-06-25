9f1b44c7bdd83bcf6b425e91b66b16311e010038
theo <theo.arnal@ynov.com>
2024-06-25 11:38:29 +0200

    tenth commit

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 export function fizzBuzz(int: number) {
+    if (int % 3 === 0 && int % 5 === 0) {
+        return "FizzBuzz";
+    }
     if (int % 3 === 0) {
         return "Fizz";
     } else if (int % 5 === 0) {
