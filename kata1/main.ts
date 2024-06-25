008921734a45a05d9a72cef85e04905a0529c44d
theo <theo.arnal@ynov.com>
2024-06-25 11:42:23 +0200

    KATA 1 finish

--- kata1/main.ts
+++ kata1/main.ts
@@ -1,8 +1,7 @@
 export function fizzBuzz(int: number) {
     if (int % 3 === 0 && int % 5 === 0) {
         return "FizzBuzz";
-    }
-    if (int % 3 === 0) {
+    } else if (int % 3 === 0) {
         return "Fizz";
     } else if (int % 5 === 0) {
         return "Buzz";
