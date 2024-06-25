8766581e728c24dcb2fb8f795f7cb473cf304b4b
theo <theo.arnal@ynov.com>
2024-06-25 11:22:37 +0200

    fourth commit

--- main.test.ts
+++ main.test.ts
@@ -4,4 +4,7 @@ describe('fizzBuzz module', () => {
     test('should return 1 in string when given 1 in integer given', () => {
         expect(fizzBuzz(1)).toBe("1");
     });
+    test('should return "Fizz" in string when mutiple of 3 integer given', () => {
+        expect(fizzBuzz(3)).toBe("Fizz");
+    });
 });
