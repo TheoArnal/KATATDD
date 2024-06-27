19d90946a5b2057d1f6e5542056b76c61aff67e4
theo <theo.arnal@ynov.com>
2024-06-27 12:03:08 +0200

    Kata 2 twenty sixth commit

--- kata2/main.ts
+++ kata2/main.ts
@@ -1,32 +1,35 @@
+
+const firstSeparator = ',';
+const pipeSeparator = '|';
+const thirdSeparator = ';';
+const fourthSeparator = 'sep';
+
+const delimiters = [firstSeparator, pipeSeparator, thirdSeparator, fourthSeparator];
+
 export function stringCalculator(string: string) {
     if (!string) return 0;
-
-    if (string.endsWith(',') || string.endsWith('\n')) {
+    if (string.endsWith(firstSeparator) || string.endsWith('\n')) {
         throw new Error('error');
     }
-
     if (string.startsWith('//')) {
-        if(string.includes('|') && string.includes(',')){
-            throw new Error('‘|’ expected but ‘,’ found at position 3.');
-        }
-        let delimiter = string[2];
-        if (string.includes('sep')){
-            delimiter = 'sep';
-            string = string.slice(6);
+        string = string.slice(2)
+        if(delimiters.some(delimiter => string.includes(delimiter))){
+            let positionDelimiter = string.search(/[,|;|sep]/);
+            if (string.includes(pipeSeparator) && string.includes(firstSeparator)){
+                throw new Error(pipeSeparator +' expected but ' + firstSeparator + ' found at position' + string.indexOf(firstSeparator) + '.');
+            }
+            if (string.includes('sep')){
+                let matchs = string.match(/\d+/g) || [];
+                return matchs.map(Number).reduce((acc, curr) => acc + curr, 0);
+            }
+            let delimiter = string[positionDelimiter];
+            string = string.slice(positionDelimiter)
             return string.split(delimiter).map(Number).reduce((acc, curr) => acc + curr, 0);
         }
-        string = string.slice(4);
-        return string
-            .split(delimiter)
-            .map(Number)
-            .reduce((acc, curr) => acc + curr, 0);
-
     }
-
-
     return string
-        .replace(/\n/g, ',')
-        .split(',')
+        .replace(/\n/g, firstSeparator)
+        .split(firstSeparator)
         .map(Number)
         .reduce((acc, curr) => acc + curr, 0);
 }
